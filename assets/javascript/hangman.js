10c8c071e41fa8e10c03ee3ec32d73c4cf8866a9
Your Name <jonathan.r.gilbert@gmail.com>
2018-04-24 10:21:25 -0700

    DRY'd the letter selection code a bit. unable to fully DRY at this time, as I haven't yet determined why THIS.ID is misbehaving

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -35,431 +35,110 @@ function blank() {
 
 $("#A").click(function () {
     choice = "a";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#B").click(function () {
     choice = "b";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#C").click(function () {
     choice = "c";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#D").click(function () {
     choice = "d";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#E").click(function () {
     choice = "e";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#F").click(function () {
     choice = "f";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#G").click(function () {
     choice = "g";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#H").click(function () {
     choice = "h";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#I").click(function () {
     choice = "i";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#J").click(function () {
     choice = "j";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#K").click(function () {
     choice = "k";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#L").click(function () {
     choice = "l";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#M").click(function () {
     choice = "m";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#N").click(function () {
     choice = "n";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#O").click(function () {
     choice = "o";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#P").click(function () {
     choice = "p";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#Q").click(function () {
     choice = "q";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#R").click(function () {
     choice = "r";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#S").click(function () {
     choice = "s";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#T").click(function () {
     choice = "t";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#U").click(function () {
     choice = "u";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#V").click(function () {
     choice = "v";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#W").click(function () {
     choice = "w";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#X").click(function () {
     choice = "x";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#Y").click(function () {
     choice = "y";
-    if (good.indexOf(choice) > -1) {
-        return null;
-    }
-    if (bad.indexOf(choice) > -1) {
-        return null;
-    }
-    else {
-        if (word.indexOf(choice) > -1) {
-            good.push(choice);
-        }
-        else
-            bad.push(choice);
-    }
     score();
 });
 $("#Z").click(function () {
     choice = "z";
+    score();
+});
+
+function score() {
     if (good.indexOf(choice) > -1) {
         return null;
     }
@@ -473,10 +152,6 @@ $("#Z").click(function () {
         else
             bad.push(choice);
     }
-    score();
-});
-
-function score() {
     if (mystery.indexOf(choice) > -1) {
         for (var i = 0; i < mystery.length; i++) {
             if (mystery[i] == choice) {
@@ -512,8 +187,8 @@ function reset() {
 function victory() {
     if (blanks.length == good.length) {
         tries = 10;
-        picNum = 5;    
-        wins++; 
+        picNum = 5;
+        wins++;
         good = [];
         bad = [];
         mystery = [];
